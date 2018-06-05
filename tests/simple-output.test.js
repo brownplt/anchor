be6a13223a8cce7c4a9f0f769d32f817d2876527
Joe Gibbs Politz <joe.politz@gmail.com>
2018-06-05 09:27:00 -0700

    super simple test setup to get things started, plus travis

---
+++ tests/simple-output.test.js
@@ -0,0 +1,31 @@
+const assert = require('assert');
+const glob = require('glob');
+const fs = require('fs');
+const path = require('path');
+const cp = require('child_process');
+
+describe("testing simple-output programs", () => {
+  const files = glob.sync("tests/simple-output/*.arr", {});
+  console.log(files);
+  files.forEach(f => {
+    test(`${f}`, () => {
+      cp.spawnSync("rm", ["-rf", "tests/compiled"]);
+      cp.spawnSync("mkdir", ["-p", "tests/compiled"]);
+      const compileProcess = cp.spawnSync("node", ["./build/pyret.jarr", "--builtin-js-dir", "src/runtime", "--runtime-path", "../../src/runtime", "--compiled-dir", "tests/compiled", f]);
+      assert(compileProcess.status === 0, `${compileProcess.stdout}\n${compileProcess.stderr}`);
+
+      const contents = String(fs.readFileSync(f));
+      const firstLine = contents.split("\n")[0];
+      const expect = firstLine.slice(firstLine.indexOf(" "));
+      console.log(contents);
+
+      const basename = path.basename(f);
+      const dest = glob.sync(`./tests/compiled/${basename}*-run.js`)[0];
+
+      console.log(expect, dest);
+      const runProcess = cp.spawnSync("node", [dest], {stdio: 'pipe'});
+      assert(runProcess.status === 0, `${runProcess.stdout}\n${runProcess.stderr}`);
+      assert(runProcess.stdout.indexOf(expect) !== -1, `${runProcess.stdout} should contain ${expect}`);
+    });
+  });
+});
